bb72630e0b642d00c965801edd0913dfa80adcac
thinski <xiamlin@gmail.com>
2015-04-06 16:58:31 +0800

    增加charts

--- navicrash.php
+++ navicrash.php
@@ -2,6 +2,38 @@
 <head >
 	<link rel="stylesheet" type="text/css" href="./css/bootstrap.css" />
 	<link rel="stylesheet" type="text/css" href="./css/test.css" />
+	<script type="text/javascript" src="./js/jquery.min.js"></script>
+  	<script type="text/javascript" src="./js/highcharts.js"></script>
+ <script type="text/javascript" >
+
+	$(function () { 
+    $('#highcharts').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'crash占比趋势'
+        },
+        xAxis: {
+            categories: ['my', 'first', 'chart']
+        },
+        yAxis: {
+            title: {
+                text: 'crash占比'
+            }
+        },
+        credits: {
+        	enabled : false
+        },
+        
+        series: [{
+            name: '时间',
+            data: [1, 0, 4]
+        }]
+        
+    });
+});			
+</script>
 
 	<title> NaviCrash </title>
 
@@ -10,7 +42,7 @@
 	<body>
 <?php require_once ("./titlebar.html"); ?>
 
-<div id="crashtable" class="updatestyle">
+<div id="crashtable" class="container">
 <?php
 	require_once './MySqlInterface.php';
 
@@ -20,7 +52,7 @@
 
 	$cr = $msql->getCrashData($sql);
 	$num = count($cr->day);
-	echo "<table class=\"table table-hover\">\n";
+	echo "<table class=\"table table-bordered\">\n";
 	echo "<tr><th>时间</th><th>导航crash数</th><th>地图crash数</th><th>导航crash占比</th><th>版本号</th></tr>";
 	for($i=0;$i<$num;$i++) {
 		echo "<tr>";
@@ -35,7 +67,9 @@
 	echo "</table>\n";
 ?>
 	</div>
-	
+	<div id="highcharts" class="container" >
+		
+</div>	
 	</body>
 
 </html>
