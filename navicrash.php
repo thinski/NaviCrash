c1f6d99564633f1edbc99e4dbda63e1db0b7a821
thinski <xiamlin@gmail.com>
2015-04-07 13:03:27 +0800

    调通折线图显示

--- navicrash.php
+++ navicrash.php
@@ -4,36 +4,7 @@
 	<link rel="stylesheet" type="text/css" href="./css/test.css" />
 	<script type="text/javascript" src="./js/jquery.min.js"></script>
   	<script type="text/javascript" src="./js/highcharts.js"></script>
- <script type="text/javascript" >
-
-	$(function () { 
-    $('#highcharts').highcharts({
-        chart: {
-            type: 'line'
-        },
-        title: {
-            text: 'crash占比趋势'
-        },
-        xAxis: {
-            categories: ['my', 'first', 'chart']
-        },
-        yAxis: {
-            title: {
-                text: 'crash占比'
-            }
-        },
-        credits: {
-        	enabled : false
-        },
-        
-        series: [{
-            name: '时间',
-            data: [1, 0, 4]
-        }]
-        
-    });
-});			
-</script>
+ 
 
 	<title> NaviCrash </title>
 
@@ -52,6 +23,8 @@
 
 	$cr = $msql->getCrashData($sql);
 	$num = count($cr->day);
+	$value_date = "[";
+	$crashrate = "[";
 	echo "<table class=\"table table-bordered\">\n";
 	echo "<tr><th>时间</th><th>导航crash数</th><th>地图crash数</th><th>导航crash占比</th><th>版本号</th></tr>";
 	for($i=0;$i<$num;$i++) {
@@ -63,12 +36,59 @@
 		echo "<td>" . $cr->sv[$i] . "</td>";
 		//echo "<td>" . $cr->os[$i] . "</td>";
 		echo "</tr>\n";
+		if($i != $num - 1) {
+			$value_date = $value_date . "'" .date('Y-m-d',strtotime($cr->day[$i])). "'" .",";
+		} else {
+			$value_date = $value_date . "'" .date('Y-m-d',strtotime($cr->day[$i])) ."'"  . "]";
+		}
+
+		if($i != $num - 1) {
+			$crashrate = $crashrate . sprintf("%.2f", $cr->rate[$i]*100) . ",";
+		} else {
+			$crashrate = $crashrate . sprintf("%.2f", $cr->rate[$i]*100) . "]";
+		}
 	}
 	echo "</table>\n";
 ?>
 	</div>
-	<div id="highcharts" class="container" >
-		
+
+	<script type="text/javascript" >
+
+	$(function () { 
+    $('#highcharts').highcharts({
+        chart: {
+            type: 'line'
+        },
+        title: {
+        	style:{
+						  fontSize: '18px',
+						  fontWeight:"bold"
+					},
+            text: 'crash占比趋势(%)'
+        },
+        xAxis: {
+            categories: <?php echo $value_date; ?>
+        },
+        yAxis: {
+            title: {
+                text: 'crash占比'
+            }
+        },
+        credits: {
+        	enabled : false
+        },
+        
+        series: [{
+            name: 'Android组件',
+            data: <?php echo $crashrate; ?>
+        }]
+        
+    });
+});			
+</script>
+
+<div id="highcharts" class="container" >
+	
 </div>	
 	</body>
 
